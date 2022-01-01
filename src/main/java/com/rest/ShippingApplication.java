1da658b423272d1fa4c86f741c3f72e4f7987d67
MuhammadAtta <muhammad.abdelmageed@gmail.com>
2022-01-01 17:51:50 +0200

    Update ShippingApplication.java

--- src/main/java/com/rest/ShippingApplication.java
+++ src/main/java/com/rest/ShippingApplication.java
@@ -6,9 +6,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
-/**
- * @author Waseem Zawaideh
- */
 @SpringBootApplication
 public class ShippingApplication {
 
