a179874f571acd9dabb3c6eebc32600580187dfb
MuhammadAtta <muhammad.abdelmageed@gmail.com>
2022-01-01 17:26:03 +0200

    Update B.java

--- src/main/java/com/rest/B.java
+++ src/main/java/com/rest/B.java
@@ -1,1 +1,16 @@
+package com.rest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@SpringBootApplication
+public class ShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingApplication.class, args);
+
+	}
+}
